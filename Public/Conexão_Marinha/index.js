172471b690537e5da4e10f0fa38c1f7e330093aa
Your Name <pedrinhopolido@gmail.com>
2024-06-10 11:50:50 -0300

    Implementação Login

---
+++ Public/Conexão_Marinha/index.js
@@ -0,0 +1,11 @@
+//LOGIN
+document.getElementById('loginButton').addEventListener('click', function() {
+    const username = prompt('Usuário:');
+    const password = prompt('Senha:');
+
+    if (username === 'admin' && password === '1234') {
+        alert('Login bem-sucedido!');
+    } else {
+        alert('Nome de usuário ou senha incorretos.');
+    }
+});
